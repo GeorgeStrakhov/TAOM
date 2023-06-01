9f6a2f43c0be8c3cc67e0da69627d049ced9235c
George Strakhov <george.strakhov@gmail.com>
2023-06-01 23:53:56 +0200

    minor layout and copy fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,11 +11,12 @@
   </div>
   <h3 class="subtitle text-center font-italic font-normal">quia multi sumus</h3>
   <div class="text-center mt-5">℘</div>
+
   <!-- benefits :1 -->
 
   <div class="grid mb-5">
     <div class="col-12 text-center">
-      <h1>Summon Your D<span class="font-italic">ai</span>mons</h1>
+      <h2>Summon Your D<span class="font-italic">ai</span>mons</h2>
     </div>
     <div class="col-12 md:col-6 lg:col-4 lg:col-offset-2 text-center md:text-right md:pr-4">
        <img class="interface-img" src="/img/daimons.jpg" alt="summon your daimons"/>
@@ -24,11 +25,10 @@
       <ul class="line-height-3 xl:text-xl">
         <li>A d<span class="underline">ai</span>mon is a machine learning agent with an LLM as a primary natural language interface (think Langchain).</li>
         <li>Each daimon by default has a master-prompt, a private long-term memory and access to the web.</li>
-        <li>The simplest kind of daimon is just a prompt that asks an LLM to impersonate a certain famous character: e.g. Feynman Daimon, Marie Curie Daimon, Lao Tzu Daimon... etc.</li>
+        <li>The simplest kind of daimon is just a prompt that asks an LLM to impersonate a certain famous character: e.g. Richar Feynman, Marie Curie, Lao Tzu... etc.</li>
         <li>Daimons can be more complex and have other ML or non-ML tools under the hood - from image generation capacity to special knowledge, skills or data access.</li>
-        <li>You can summon daimons from an existing library or create your own by simply specifying a master prompt or by writing your own arbitrarily complex agents in code.</li>
+        <li>You can summon daimons from an existing library or create your own by simply specifying a master-prompt or by writing your own arbitrarily complex agents in code.</li>
         <li>You can fork and customize daimons.</li>
-        <li>Daimons can stay awake all the time or wake up only when @mentioned.</li>
       </ul>
     </div>
   </div>
@@ -37,7 +37,7 @@
 
   <div class="grid my-5">
     <div class="col-12 text-center">
-      <h1>Weave Your Threads</h1>
+      <h2>Weave Your Threads</h2>
     </div>
     <div class="md:hidden col-12 md:col-6 lg:col-4 lg:col-offset-2 text-center md:text-right md:pr-4">
        <img class="interface-img" src="/img/threads.jpg" alt="weave your threads"/>
@@ -45,10 +45,10 @@
     <div class="col-12 md:col-6 lg:col-4 lg:col-offset-2 lg:pl-5 flex flex-column align-content-center justify-content-center">
         <ul class="line-height-3 xl:text-xl">
           <li>A thread is a single train of thought, context, project, conversation.</li>
-          <li>Each thread has a descriptive name, a set of daimons currently present, and (optionally) attached data - like a spreadsheet or a document, or a piece of code that all thread participants are working collaboratively on (reading and/or writing).</li>
-          <li>When you summon a new daimon to a thread - you can define a role they will play in this thread by simply addressing them, e.g. <span class="font-italic">"@Socrates, please keep us honest here"</span></li>
+          <li>Each thread has a set of daimons present and attached data, like a document, or a piece of code that all thread participants are working collaboratively on.</li>
+          <li>When you summon a new daimon to a thread - you can define the role they should play in this thread by simply addressing them, e.g. <span class="font-italic">"@Socrates, please keep us honest"</span></li>
           <li>You can ask various simpler daimons to act as a summarizer, a moderator, a time-keeper in a thread etc.</li>
-          <li>If a certain daimon in a thread is asked to stay awake - then they will run at regular intervals and will proactively say things in a thread and do things in the documents/data, @mentioning other daimons or you</li>
+          <li>Daimons can stay awake (running the prompt automatically with interval) in the thread or only wake up when @mentioned.</li>
           <li>You can invite real people to join your threads too.</li>
         </ul>
     </div>
@@ -61,7 +61,7 @@
 
   <div class="grid my-5">
     <div class="col-12 text-center">
-      <h1>Save Your Soul</h1>
+      <h2>Save Your Soul</h2>
     </div>
     <div class="col-12 md:col-6 lg:col-4 lg:col-offset-2 text-center md:text-right md:pr-4">
        <img class="interface-img" src="/img/soul.jpg" alt="save your soul"/>
